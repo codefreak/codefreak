660e9e556fc0df567f37c0f9cc1094ab3a2931a0
Erik Hofer <erik-hofer@mail.de>
2019-03-14 17:08:46 +0100

    create project with Spring initializr

---
+++ src/main/kotlin/de/code_freak/codefreak/CodeFreakApplication.kt
@@ -0,0 +1,11 @@
+package de.code_freak.codefreak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeFreakApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodeFreakApplication>(*args)
+}
---
+++ src/test/kotlin/de/code_freak/codefreak/CodeFreakApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.code_freak.codefreak
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CodeFreakApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
