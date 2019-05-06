a9a33bfddfe44c67926442a64ab959747e98cd5e
Henning <henning.kasch@fh-kiel.de>
2019-05-06 15:54:12 +0200

    Fix executable bit for files inside tars

--- src/test/kotlin/de/code_freak/codefreak/util/TarUtilTest.kt
+++ src/test/kotlin/de/code_freak/codefreak/util/TarUtilTest.kt
@@ -2,6 +2,7 @@ package de.code_freak.codefreak.util
 
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.Test
 import org.springframework.core.io.ClassPathResource
@@ -14,7 +15,17 @@ internal class TarUtilTest {
     val tar = TarUtil.createTarFromDirectory(ClassPathResource("util/tar-sample").file)
     TarArchiveInputStream(ByteArrayInputStream(tar)).use {
       val result = generateSequence { it.nextTarEntry }.map { it.name }.toList()
-      assertThat(result, containsInAnyOrder("./", "./foo.txt", "./subdir/", "./subdir/bar.txt"))
+      assertThat(result, containsInAnyOrder("./", "./executable.sh", "./foo.txt", "./subdir/", "./subdir/bar.txt"))
+    }
+  }
+
+  @Test
+  fun `tar persists execute permissions`() {
+    val tar = TarUtil.createTarFromDirectory(ClassPathResource("util/tar-sample").file)
+    TarArchiveInputStream(ByteArrayInputStream(tar)).use {
+      val result = generateSequence { it.nextTarEntry }.filter { it.name == "./executable.sh" }.first()
+      // octal 100744 = int 33252
+      assertThat(result.mode, `is`(33252))
     }
   }
 }
