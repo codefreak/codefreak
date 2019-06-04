8abc117c73d74eb2f81fbd60ad684f9d57af9e26
Henning <henning.kasch@fh-kiel.de>
2019-06-04 10:08:14 +0200

    Add pdf of submission to admin export

--- src/main/kotlin/de/code_freak/codefreak/service/AssignmentService.kt
+++ src/main/kotlin/de/code_freak/codefreak/service/AssignmentService.kt
@@ -75,6 +75,9 @@ class AssignmentService {
       // write a meta-file with information about user
       val metaFile = File(submissionDir, "freak.json")
       metaFile.writeText(Klaxon().toJsonString(it.user))
+      // write pdf with submission
+      val pdfFile = File(submissionDir, "submission.pdf")
+      pdfFile.writeBytes(latexService.submissionToPdf(it))
     }
 
     val archive = TarUtil.createTarFromDirectory(tmpDir)
