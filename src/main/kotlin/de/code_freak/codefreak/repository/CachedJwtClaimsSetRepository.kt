dfb4d152d02570a3a6ebb199881d6bc1349ed112
Henning <henning.kasch@fh-kiel.de>
2019-08-26 13:58:38 +0200

    Fix linting issues

--- src/main/kotlin/de/code_freak/codefreak/repository/CachedJwtClaimsSetRepository.kt
+++ src/main/kotlin/de/code_freak/codefreak/repository/CachedJwtClaimsSetRepository.kt
@@ -6,4 +6,4 @@ import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface CachedJwtClaimsSetRepository: CrudRepository<CachedJwtClaimsSet, UUID>
+interface CachedJwtClaimsSetRepository : CrudRepository<CachedJwtClaimsSet, UUID>
