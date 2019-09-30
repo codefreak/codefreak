af91444ee161ffddaefc3fc340b6c98694ff919d
Erik Hofer <erik-hofer@mail.de>
2019-09-30 13:56:27 +0200

    Remove external dependencies from entity layer

--- src/main/kotlin/de/code_freak/codefreak/entity/CachedJwtClaimsSet.kt
+++ src/main/kotlin/de/code_freak/codefreak/entity/CachedJwtClaimsSet.kt
@@ -1,18 +1,12 @@
 package de.code_freak.codefreak.entity
 
-import com.nimbusds.jwt.JWTClaimsSet
 import org.hibernate.annotations.Type
 import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
-class CachedJwtClaimsSet(initClaimSet: JWTClaimsSet? = null) : BaseEntity() {
-  @Column
-  @Type(type = "text")
-  var serializedClaimSet = initClaimSet.toString()
-
-  @delegate:Transient
-  val jwtClaimsSet: JWTClaimsSet by lazy {
-    initClaimSet ?: JWTClaimsSet.parse(serializedClaimSet)
-  }
-}
+class CachedJwtClaimsSet(
+    @Column
+    @Type(type = "text")
+    var serializedClaimSet: String
+) : BaseEntity()
