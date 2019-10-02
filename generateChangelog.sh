609da9de3a863052d1f34f4c058c27bc1df3b751
Henning <henning.kasch@fh-kiel.de>
2019-10-02 14:50:34 +0200

    Generate changelog in resources with timestamp and summary

--- generateChangelog.sh
+++ generateChangelog.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+date=$(date '+%Y%m%d%H%M%S')
+echo "Please enter a summary for this migration: "
+read summary
+filename=${date}-$(echo "$summary" | sed -e 's/[^A-Za-z0-9._]+/_/g' | tr '[:upper:]' '[:lower:]').yaml
 ./gradlew clean assemble liquibaseUpdate
-./gradlew liquibaseDiffChangelog -PchangeLogFile=build/db/changelog-diff.yaml
+./gradlew liquibaseDiffChangelog -PchangeLogFile=src/main/resources/db/changelogs/$filename
+cat << EOL >> src/main/resources/db/changelog-master.yaml
+- include:
+    file: changelogs/$filename
+    relativeToChangelogFile: true
+EOL
