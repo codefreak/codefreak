80380cacdcfeb4d5edc863858e5fcb1f6f187855
Erik Hofer <erik-hofer@mail.de>
2019-10-02 17:00:01 +0200

    Fix changelog scrip for Windows; add documentation

--- generateChangelog.sh
+++ generateChangelog.sh
@@ -4,7 +4,7 @@ set -e
 date=$(date '+%Y%m%d%H%M%S')
 echo "Please enter a summary for this migration: "
 read summary
-filename=${date}-$(echo "$summary" | sed -e 's/[^A-Za-z0-9._]+/_/g' | tr '[:upper:]' '[:lower:]').yaml
+filename=${date}-$(echo "$summary" | sed -r 's/[^A-Za-z0-9._]+/-/g' | tr '[:upper:]' '[:lower:]').yaml
 ./gradlew clean assemble liquibaseUpdate
 ./gradlew liquibaseDiffChangelog -PchangeLogFile=src/main/resources/db/changelogs/$filename
 cat << EOL >> src/main/resources/db/changelog-master.yaml
