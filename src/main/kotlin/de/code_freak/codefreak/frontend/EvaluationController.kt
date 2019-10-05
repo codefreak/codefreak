3f961400580ccd060421ead3c8310bb05dfa8e46
Henning Kasch <henning.kasch@fh-kiel.de>
2019-10-05 20:26:45 +0200

    Restrict access to evaluation results

--- src/main/kotlin/de/code_freak/codefreak/frontend/EvaluationController.kt
+++ src/main/kotlin/de/code_freak/codefreak/frontend/EvaluationController.kt
@@ -1,8 +1,11 @@
 package de.code_freak.codefreak.frontend
 
+import de.code_freak.codefreak.auth.Authority
+import de.code_freak.codefreak.auth.Role
 import de.code_freak.codefreak.service.evaluation.EvaluationService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import java.util.UUID
+import javax.ws.rs.ForbiddenException
 
 @Controller
 class EvaluationController : BaseController() {
@@ -36,7 +40,9 @@ class EvaluationController : BaseController() {
   @GetMapping("/evaluations/{evaluationId}")
   fun getEvaluation(@PathVariable("evaluationId") evaluationId: UUID, model: Model): String {
     val evaluation = evaluationService.getEvaluation(evaluationId)
-    // TODO authorization
+    if(!user.authorities.contains(Role.TEACHER) && evaluation.answer.submission.user != user.entity) {
+      throw AccessDeniedException("Cannot access evaluation")
+    }
     val resultTemplates = mutableMapOf<UUID, String>()
     val resultContents = mutableMapOf<UUID, Any>()
     evaluation.results.forEach {
