719f8eacb226d47ad250fb80d9e9065b6446d71b
Erik Hofer <erik-hofer@mail.de>
2019-12-19 17:22:52 +0100

    Add background for login page

--- client/src/components/Centered.tsx
+++ client/src/components/Centered.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Centered: React.FC<{ style?: React.CSSProperties }> = props => {
+const Centered: React.FC<{
+  style?: React.CSSProperties
+  className?: string
+}> = props => {
   const style = {
     height: '100%',
     display: 'flex',
@@ -8,7 +11,11 @@ const Centered: React.FC<{ style?: React.CSSProperties }> = props => {
     alignItems: 'center',
     ...props.style
   }
-  return <div style={style}>{props.children}</div>
+  return (
+    <div style={style} className={props.className}>
+      {props.children}
+    </div>
+  )
 }
 
 export default Centered
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -5,6 +5,18 @@ import Centered from '../components/Centered'
 import { AuthenticatedUser } from '../hooks/useAuthenticatedUser'
 import { useLoginMutation } from '../services/codefreak-api'
 
+const backgroundStyle = `
+background:
+linear-gradient(27deg, #151515 5px, transparent 5px) 0 5px,
+linear-gradient(207deg, #151515 5px, transparent 5px) 10px 0px,
+linear-gradient(27deg, #222 5px, transparent 5px) 0px 10px,
+linear-gradient(207deg, #222 5px, transparent 5px) 10px 5px,
+linear-gradient(90deg, #1b1b1b 10px, transparent 10px),
+linear-gradient(#1d1d1d 25%, #1a1a1a 25%, #1a1a1a 50%, transparent 50%, transparent 75%, #242424 75%, #242424);
+background-color: #131313;
+background-size: 20px 20px;
+`
+
 interface Credentials {
   username: string
   password: string
@@ -36,7 +48,7 @@ const LoginPage: React.FC<LoginProps> = props => {
   }
 
   return (
-    <Centered style={{ background: 'grey' }}>
+    <Centered className="background-carbon">
       <Card title="Login" style={{ width: '100%', maxWidth: 300, margin: 16 }}>
         <Form onSubmit={handleSubmit}>
           <Form.Item>
