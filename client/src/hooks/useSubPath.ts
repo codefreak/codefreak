3fe864809cc66f07dcb4e2b0afad65437e7aceb8
Erik Hofer <erik-hofer@mail.de>
2020-01-20 18:46:09 +0100

    Add tabs to evaluation page

--- client/src/hooks/useSubPath.ts
+++ client/src/hooks/useSubPath.ts
@@ -1,11 +1,19 @@
 import { useHistory, useLocation, useRouteMatch } from 'react-router'
 
-const useSubPath = (replace: boolean = true) => {
+/**
+ * Control the path relative to the current route
+ * @param replace when setting the subPath, should it be replaced or pushed in the history
+ * @param deep when getting the subPath, include all levels below the current (true) or only the first level (false)
+ */
+const useSubPath = (replace: boolean = true, deep: boolean = false) => {
   const { url } = useRouteMatch()
   const { pathname } = useLocation()
   const history = useHistory()
   return {
-    get: () => pathname.substring(url.length),
+    get: () => {
+      const fullPath = pathname.substring(url.length)
+      return deep || fullPath === '' ? fullPath : '/' + fullPath.split('/')[1]
+    },
     set: (path: string) =>
       replace ? history.replace(url + path) : history.push(url + path)
   }
--- client/src/pages/evaluation/EvaluationPage.tsx
+++ client/src/pages/evaluation/EvaluationPage.tsx
@@ -1,14 +1,18 @@
-import { Button, Empty, Steps } from 'antd'
+import { Button, Empty, Icon, Result, Steps, Tabs } from 'antd'
 import React from 'react'
+import { useRouteMatch } from 'react-router-dom'
 import AsyncPlaceholder from '../../components/AsyncContainer'
+import useSubPath from '../../hooks/useSubPath'
 import {
   useGetEvaluationOverviewQuery,
   useStartEvaluationMutation
 } from '../../services/codefreak-api'
 
 const { Step } = Steps
+const { TabPane } = Tabs
 
 const EvaluationPage: React.FC<{ answerId: string }> = ({ answerId }) => {
+  const subPath = useSubPath()
   const result = useGetEvaluationOverviewQuery({ variables: { answerId } })
 
   const [startEvaluation, startEvaluationResult] = useStartEvaluationMutation({
@@ -32,24 +36,39 @@ const EvaluationPage: React.FC<{ answerId: string }> = ({ answerId }) => {
         />
         <Step title="Waiting" description="This is a description." />
       </Steps>
-      <Button
-        type="primary"
-        size="large"
-        icon="caret-right"
-        onClick={startEvaluation as () => void}
-        loading={startEvaluationResult.loading}
-      >
-        Start Evaluation
-      </Button>
 
       <br />
       <br />
-      <h2>Latest Evaluation</h2>
-      {answer.latestEvaluation ? (
-        'TODO'
-      ) : (
-        <Empty description="The evaluation has not been run yet" />
-      )}
+      <Tabs defaultActiveKey={subPath.get()} onChange={subPath.set}>
+        <TabPane tab="Latest Evaluation" key="">
+          {answer.latestEvaluation ? (
+            'TODO'
+          ) : (
+            <Result
+              icon={<Icon type="rocket" theme="twoTone" />}
+              title="Wondering if your solution is correct? ✨"
+              extra={
+                <Button
+                  type="primary"
+                  size="large"
+                  icon="caret-right"
+                  onClick={startEvaluation as () => void}
+                  loading={startEvaluationResult.loading}
+                >
+                  Start Evaluation
+                </Button>
+              }
+            />
+          )}
+        </TabPane>
+        <TabPane
+          tab="Evaluation History"
+          disabled={!answer.latestEvaluation}
+          key="/history"
+        >
+          Content of Tab Pane 2
+        </TabPane>
+      </Tabs>
     </>
   )
 }
