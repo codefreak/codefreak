6278cebdd09b0366d4f6a60b541d5095b608832c
Erik Hofer <erik-hofer@mail.de>
2020-01-25 11:07:48 +0100

    Fix tests

--- src/test/kotlin/de/code_freak/codefreak/auth/AuthorizationTest.kt
+++ src/test/kotlin/de/code_freak/codefreak/auth/AuthorizationTest.kt
@@ -2,15 +2,31 @@ package de.code_freak.codefreak.auth
 
 import de.code_freak.codefreak.SpringFrontendTest
 import de.code_freak.codefreak.frontend.BaseController
+import de.code_freak.codefreak.service.SeedDatabaseService
+import de.code_freak.codefreak.service.UserService
 import org.junit.Test
 import org.springframework.security.access.annotation.Secured
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+
+@Component
+class TestUserDetailsService(private val userService: UserService) : UserDetailsService {
+  override fun loadUserByUsername(username: String): UserDetails = when (username) {
+    "admin" -> SeedDatabaseService.admin
+    "teacher" -> SeedDatabaseService.teacher
+    "student" -> SeedDatabaseService.student
+    else -> throw UsernameNotFoundException("User $username cannot be found")
+  }
+}
 
 @Controller
 @ResponseBody
@@ -38,7 +54,7 @@ class AuthorizationTestController : BaseController() {
 class AuthorizationTest : SpringFrontendTest() {
 
   @Test
-  @WithUserDetails("admin")
+  @WithUserDetails(value = "admin", userDetailsServiceBeanName = "testUserDetailsService")
   fun requestWithAdminUser() {
     mockMvc.perform(get("/allowRolesTest/allowAdmin")).andExpect(status().isOk)
     mockMvc.perform(get("/allowRolesTest/allowTeacher")).andExpect(status().isOk)
@@ -46,7 +62,7 @@ class AuthorizationTest : SpringFrontendTest() {
   }
 
   @Test
-  @WithUserDetails("teacher")
+  @WithUserDetails(value = "teacher", userDetailsServiceBeanName = "testUserDetailsService")
   fun requestWithTeacherUser() {
     mockMvc.perform(get("/allowRolesTest/allowAdmin")).andExpect(status().isForbidden)
     mockMvc.perform(get("/allowRolesTest/allowTeacher")).andExpect(status().isOk)
@@ -54,7 +70,7 @@ class AuthorizationTest : SpringFrontendTest() {
   }
 
   @Test
-  @WithUserDetails("student")
+  @WithUserDetails(value = "student", userDetailsServiceBeanName = "testUserDetailsService")
   fun requestWithStudentUser() {
     mockMvc.perform(get("/allowRolesTest/allowAdmin")).andExpect(status().isForbidden)
     mockMvc.perform(get("/allowRolesTest/allowTeacher")).andExpect(status().isForbidden)
