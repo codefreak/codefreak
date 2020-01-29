db69c544aff866f9c4fe9c096591b4f703ef7d42
Henning <henning.kasch@fh-kiel.de>
2020-01-29 13:29:41 +0100

    Add evaluation view for teachers

--- client/src/components/SubmissionsTable.tsx
+++ client/src/components/SubmissionsTable.tsx
@@ -1,9 +1,11 @@
-import { Button, Col, Icon, Input, Row, Table, Tooltip } from 'antd'
+import { Button, Col, Icon, Input, Modal, Row, Table, Tooltip } from 'antd'
 import React, { ChangeEvent, useState } from 'react'
 import {
   EvaluationStepResult,
   GetAssignmentWithSubmissionsQueryResult
 } from '../generated/graphql'
+import { displayName } from '../services/user'
+import EvaluationResult from './EvaluationResult'
 import EvaluationStepResultIcon from './EvaluationStepResultIcon'
 import './SubmissionsTable.less'
 
@@ -123,15 +125,58 @@ const renderAnswer = (task: Task, submission: Submission) => {
   }
 
   if (!answer.latestEvaluation) {
-    return <Icon type="question-circle" />
+    return (
+      <Tooltip title="Answer has not been evaluated, yet">
+        <Icon type="question-circle" />
+      </Tooltip>
+    )
   }
 
   return (
-    <div className="evaluation-step-results">
-      {answer.latestEvaluation.steps.map(stepResult => (
-        <EvaluationStepResultIcon key={stepResult.id} stepResult={stepResult} />
-      ))}
-    </div>
+    <EvaluationStepOverview
+      task={task}
+      user={submission.user}
+      evaluation={answer.latestEvaluation}
+    />
+  )
+}
+
+const EvaluationStepOverview: React.FC<{
+  task: Task
+  user: Submission['user']
+  evaluation: NonNullable<Answer['latestEvaluation']>
+}> = ({ evaluation, task, user }) => {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const openModal = () => setModalOpen(true)
+  const closeModal = () => setModalOpen(false)
+
+  return (
+    <>
+      <Modal
+        visible={modalOpen}
+        title={`Evaluation Results for ${task.title} of ${displayName(user)}`}
+        onCancel={closeModal}
+        footer={null}
+        width="75%"
+        bodyStyle={{ backgroundColor: '#f0f2f5' }}
+      >
+        <EvaluationResult evaluationId={evaluation.id} />
+      </Modal>
+      <div className="evaluation-step-results">
+        {evaluation.steps.map(stepResult => (
+          <EvaluationStepResultIcon
+            key={stepResult.id}
+            stepResult={stepResult}
+          />
+        ))}
+        <Tooltip title="Show evaluation result">
+          <Button type="primary" icon="bars" onClick={openModal}>
+            Details
+          </Button>
+        </Tooltip>
+      </div>
+    </>
   )
 }
 
--- client/src/services/user.ts
+++ client/src/services/user.ts
@@ -1,12 +1,16 @@
-import { AuthenticatedUser } from '../hooks/useAuthenticatedUser'
+interface UserNameProperties {
+  firstName?: string | null
+  lastName?: string | null
+  username: string
+}
 
-export const displayName = (user: AuthenticatedUser) => {
+export const displayName = (user: UserNameProperties) => {
   if (user.firstName || user.lastName) {
     return [user.firstName, user.lastName].join(' ')
   } else {
     return user.username
   }
 }
-export const initials = (user: AuthenticatedUser) => {
+export const initials = (user: UserNameProperties) => {
   return displayName(user).replace(/(\w)\w+\s*/g, '$1')
 }
