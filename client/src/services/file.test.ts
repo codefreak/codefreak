9e257c3aa97e4ce30648ace04d23c96f226af0fc
Henning Kasch <henning.kasch@fh-kiel.de>
2020-04-20 12:50:57 +0200

    Handle missing root directory in file tree
    (cherry picked from commit d96cd097ca7ec1ee614d367d539385ec566ffc66)

--- client/src/services/file.test.ts
+++ client/src/services/file.test.ts
@@ -46,31 +46,54 @@ test('abspath adds leading slash', () => {
   expect(abspath('/a/b')).toBe('/a/b')
 })
 
-test('children get nested correctly', () => {
-  const root: File = { ...dir, path: './' }
-  const file1: File = { ...file, path: './a' }
-  const dir1: File = { ...dir, path: './b' }
-  const dir2: File = { ...dir, path: './aa' }
-  const file2: File = { ...file, path: './b/b' }
+describe('fileListToTree', () => {
+  test('children get nested correctly', () => {
+    const root: File = { ...dir, path: './' }
+    const file1: File = { ...file, path: './a' }
+    const dir1: File = { ...dir, path: './b' }
+    const dir2: File = { ...dir, path: './aa' }
+    const file2: File = { ...file, path: './b/b' }
 
-  const rootNode = fileListToTree([root, file1, file2, dir1, dir2])
-  expect(rootNode).toBeDefined()
-  if (rootNode) {
-    expect(rootNode.path).toBe('./')
-    expect(rootNode).toHaveProperty('children')
-    if ('children' in rootNode && rootNode.children) {
-      expect(rootNode.children).toHaveLength(3)
-      expect(rootNode.children[0].path).toBe(file1.path)
-      expect(rootNode.children[1].path).toBe(dir2.path)
-      expect(rootNode.children[1]).toHaveProperty('children')
-      expect(rootNode.children[2].path).toBe(dir1.path)
-      expect(rootNode.children[2]).toHaveProperty('children')
-      if ('children' in rootNode.children[2] && rootNode.children[2].children) {
-        expect(rootNode.children[2].children).toHaveLength(1)
-        expect(rootNode.children[2].children[0].path).toBe(file2.path)
+    const rootNode = fileListToTree([root, file1, file2, dir1, dir2])
+    expect(rootNode).toBeDefined()
+    if (rootNode) {
+      expect(rootNode.path).toBe('./')
+      expect(rootNode).toHaveProperty('children')
+      if ('children' in rootNode && rootNode.children) {
+        expect(rootNode.children).toHaveLength(3)
+        expect(rootNode.children[0].path).toBe(file1.path)
+        expect(rootNode.children[1].path).toBe(dir2.path)
+        expect(rootNode.children[1]).toHaveProperty('children')
+        expect(rootNode.children[2].path).toBe(dir1.path)
+        expect(rootNode.children[2]).toHaveProperty('children')
+        if (
+          'children' in rootNode.children[2] &&
+          rootNode.children[2].children
+        ) {
+          expect(rootNode.children[2].children).toHaveLength(1)
+          expect(rootNode.children[2].children[0].path).toBe(file2.path)
+        }
       }
     }
-  }
+  })
+
+  test('creates missing parent dirs', () => {
+    // misses root directory
+    const dir1: File = { ...dir, path: 'bin/' }
+    const dir1file1: File = { ...file, path: 'bin/file' }
+    const dir2: File = { ...dir, path: 'aa' }
+
+    const rootNode = fileListToTree([dir1, dir1file1, dir2])
+
+    expect(rootNode).toBeDefined()
+    if (rootNode) {
+      expect(rootNode.path).toBe('/')
+      expect(rootNode).toHaveProperty('children')
+      if ('children' in rootNode && rootNode.children) {
+        expect(rootNode.children).toHaveLength(2)
+      }
+    }
+  })
 })
 
 test('numberOfLines', () => {
