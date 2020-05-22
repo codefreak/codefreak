1c9787536039c2db0a3a2da2b044cac4cbb337d5
Henning Kasch <henning.kasch@fh-kiel.de>
2020-05-22 10:54:41 +0200

    Refactor frontend version display

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,7 +1,11 @@
+const { DefinePlugin } = require('webpack')
 const CracoAntDesignPlugin = require('craco-antd')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const rawLoader = require('craco-raw-loader')
 const CopyPlugin = require('copy-webpack-plugin')
+const GitRevisionPlugin = require('git-revision-webpack-plugin')
+
+const gitRevisionPlugin = new GitRevisionPlugin()
 
 module.exports = {
   plugins: [
@@ -17,7 +21,13 @@ module.exports = {
           to: 'fonts',
           flatten: true
         }
-      ])
+      ]),
+      new GitRevisionPlugin(),
+      new DefinePlugin({
+        'process.env.BUILD_YEAR': JSON.stringify(new Date().getFullYear()),
+        'process.env.BUILD_VERSION': JSON.stringify(gitRevisionPlugin.version()),
+        'process.env.BUILD_HASH': JSON.stringify(gitRevisionPlugin.commithash())
+      })
     ]
   }
 }
