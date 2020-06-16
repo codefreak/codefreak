5c7c7005f0a8392f4b545fa836c23c1e6fa64df0
Erik Hofer <erik-hofer@mail.de>
2020-06-16 15:12:22 +0200

    Reload page on 401 to show login dialog

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -37,10 +37,22 @@ const resetWebsocket = () => {
   websocket.connect()
 }
 
+/** If not authenticated or session expired, reload page to show login dialog */
+const reloadIfNotAuthenticated = (error: any) => {
+  if (
+    error.networkError &&
+    error.networkError.extensions &&
+    error.networkError.extensions.errorCode === '401'
+  ) {
+    window.location.reload()
+  }
+}
+
 const apolloClient = new ApolloClient({
   link: ApolloLink.from([
     onError(error => {
       if (!error.operation.getContext().disableGlobalErrorHandling) {
+        reloadIfNotAuthenticated(error)
         messageService.error(extractErrorMessage(error))
       }
     }),
