b876788fb31b0f5f92b3881c27a3f4774ca86872
Henning <henning.kasch@fh-kiel.de>
2020-08-04 14:53:12 +0200

    Make short circuit ObjectMapper work again

--- src/main/kotlin/org/codefreak/codefreak/config/GraphQLConfiguration.kt
+++ src/main/kotlin/org/codefreak/codefreak/config/GraphQLConfiguration.kt
@@ -1,8 +1,13 @@
 package org.codefreak.codefreak.config
 
+import com.expediagroup.graphql.execution.KotlinDataFetcherFactoryProvider
+import com.expediagroup.graphql.execution.SimpleKotlinDataFetcherFactoryProvider
+import org.codefreak.codefreak.graphql.ShortCircuitObjectMapper
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter
 
 @Configuration
@@ -16,4 +21,17 @@ class GraphQLConfiguration {
     filter = OpenEntityManagerInViewFilter()
     addUrlPatterns("/graphql")
   }
+
+  /**
+   * Do not introduce as "ObjectMapper" because this would replace the global jackson object mapper
+   */
+  @Bean("shortCutObjectMapper")
+  fun shortCutJacksonObjectMapper(
+    builder: Jackson2ObjectMapperBuilder
+  ): ShortCircuitObjectMapper = ShortCircuitObjectMapper().also { builder.configure(it) }
+
+  @Bean
+  fun dataFetcherProvider(
+    @Qualifier("shortCutObjectMapper") objectMapper: ShortCircuitObjectMapper
+  ): KotlinDataFetcherFactoryProvider = SimpleKotlinDataFetcherFactoryProvider(objectMapper)
 }
--- src/main/kotlin/org/codefreak/codefreak/graphql/ShortCircuitObjectMapper.kt
+++ src/main/kotlin/org/codefreak/codefreak/graphql/ShortCircuitObjectMapper.kt
@@ -1,18 +1,25 @@
 package org.codefreak.codefreak.graphql
 
+import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.catalina.core.ApplicationPart
 
+/**
+ * Custom ObjectMapper brings back the short-cut behaviour of Jackson pre v2.10
+ * If fromValue is already of the correct type no further conversion is done
+ *
+ * We need this in GraphQL to make uploads work properly
+ */
 class ShortCircuitObjectMapper : ObjectMapper() {
-
   @Suppress("UNCHECKED_CAST")
-  override fun <T : Any?> convertValue(fromValue: Any?, toValueType: Class<T>?): T {
+  override fun _convert(fromValue: Any?, toValueType: JavaType?): Any? {
     if (fromValue?.javaClass == toValueType) {
-      return fromValue as T
+      return fromValue
     }
-    if (toValueType == Array<ApplicationPart>::class.java) {
-      return (fromValue as List<ApplicationPart>).toTypedArray() as T
+    // This should be more generic but we don't know the inner type of fromValue
+    if (toValueType?.rawClass == Array<ApplicationPart>::class.java) {
+      return (fromValue as List<ApplicationPart>).toTypedArray()
     }
-    return super.convertValue(fromValue, toValueType)
+    return super._convert(fromValue, toValueType)
   }
 }
