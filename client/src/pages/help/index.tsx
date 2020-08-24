bafae44fa2f71fb56658282c1636a71aa884421c
Erik Hofer <erik-hofer@mail.de>
2020-08-24 14:09:51 +0200

    Fix linting issues

--- client/src/pages/help/index.tsx
+++ client/src/pages/help/index.tsx
@@ -4,6 +4,6 @@ export const HelpPage = loadable(() => import('./HelpPage'))
 
 export const BasicHelpPage = loadable(() => import('./BasicHelpPage'))
 
-export const DefinitionsHelpPage = loadable(() =>
-  import('./DefinitionsHelpPage')
+export const DefinitionsHelpPage = loadable(
+  () => import('./DefinitionsHelpPage')
 )
