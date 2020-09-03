6331dd3988da45bede98eca98c335edec9370073
Erik Hofer <erik-hofer@mail.de>
2020-09-03 16:00:20 +0200

    Adapt git import height

--- client/src/components/FileImport.tsx
+++ client/src/components/FileImport.tsx
@@ -69,7 +69,7 @@ const FileImport: React.FC<FileImportProps> = props => {
         </Dragger>
       </Col>
       <Col span={12}>
-        <Centered style={{ height: 150 }}>
+        <Centered style={{ height: 170 }}>
           <Form
             layout="horizontal"
             style={{ width: '100%', maxWidth: 700, marginBottom: -24 }}
