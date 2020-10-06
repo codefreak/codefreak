1d48681044ee5ff49ca8db8cad3f2a3cabf5ada6
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2020-10-06 11:18:26 +0200

    Create component for a sorted list

---
+++ client/src/components/SortedList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface SortedListProps<T> {
+  list: T[],
+  sort: (a: T, b: T) => number,
+  render: (a: T) => JSX.Element
+}
+
+const SortedList = <T extends unknown>(props: SortedListProps<T>) => (
+  <>
+    {props.list.slice()
+      .sort(props.sort)
+      .map(props.render)}
+  </>
+)
+
+export default SortedList
