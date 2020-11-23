d1cdd990107597315d7bf91db38917c9e50fd238
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2020-11-23 13:59:35 +0100

    Don't touch existing tasks when importing

--- src/main/kotlin/org/codefreak/codefreak/service/TaskTarService.kt
+++ src/main/kotlin/org/codefreak/codefreak/service/TaskTarService.kt
@@ -3,7 +3,6 @@ package org.codefreak.codefreak.service
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.time.Instant
 import java.util.UUID
 import liquibase.util.StreamUtil
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry
@@ -20,11 +19,9 @@ import org.codefreak.codefreak.service.evaluation.runner.CommentRunner
 import org.codefreak.codefreak.service.file.FileService
 import org.codefreak.codefreak.util.TarUtil
 import org.codefreak.codefreak.util.TarUtil.getCodefreakDefinition
-import org.codefreak.codefreak.util.UuidUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.core.io.ClassPathResource
-import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -49,33 +46,18 @@ class TaskTarService : BaseService() {
 
   /**
    * Creates and saves a new task from the given tar.
-   * If the task already exists in the repository and the tar content is newer,
-   * no new task is created and the existing task is updated instead.
    *
    * @param tarContent the tar containing a task
    * @param owner the owner of the task
    * @param assignment the assignment the task belongs to, if any
    * @param position the position of the task if it belongs to an assignment
-   * @return the created or updated task
+   * @return the created task
    */
   @Transactional
   fun createFromTar(tarContent: ByteArray, owner: User, assignment: Assignment? = null, position: Long = 0L): Task {
     val definition = yamlMapper.getCodefreakDefinition<TaskDefinition>(tarContent.inputStream())
 
-    val existingTask = try {
-      val taskId = UuidUtil.parse(definition.id)
-      taskId?.let {
-        id -> taskService.findTask(id).takeIf { it.owner == owner && it.assignment == assignment }
-      }
-    } catch (e: EntityNotFoundException) {
-      null
-    }
-
-    var task = if (existingTask == null) {
-      createNewTaskFromDefinition(definition, owner, assignment, position)
-    } else {
-      updateExistingTaskFromTar(existingTask, definition)
-    }
+    var task = createNewTaskFromDefinition(definition, owner, assignment, position)
 
     addBuiltInEvaluationSteps(task)
     evaluationStepDefinitionRepository.saveAll(task.evaluationStepDefinitions)
@@ -84,60 +66,6 @@ class TaskTarService : BaseService() {
     copyTaskFilesFromTar(task.id, tarContent)
 
     return task
-  }
-
-  /**
-   * Updates an existing task with the information provided in the TaskDefinition.
-   * Also updates all evaluation step definitions of the task.
-   * Saves the task to the repository in the process.
-   */
-  private fun updateExistingTaskFromTar(task: Task, definition: TaskDefinition): Task {
-    val updatedAt = Instant.parse(definition.updatedAt)
-    val isUpToDate = !updatedAt.isAfter(task.updatedAt)
-
-    if (isUpToDate) {
-      return task
-    }
-
-    var updatedTask = task
-
-    updatedTask.title = definition.title
-    updatedTask.body = definition.description
-    updatedTask.hiddenFiles = definition.hidden
-    updatedTask.protectedFiles = definition.protected
-    updatedTask.ideEnabled = definition.ide?.enabled ?: true
-    updatedTask.ideImage = definition.ide?.image
-
-    updatedTask = taskService.saveTask(updatedTask)
-
-    val stepDefinitions = getEvaluationStepDefinitions(definition, task)
-
-    updateExistingEvaluationStepDefinitions(updatedTask, stepDefinitions)
-
-    val newStepDefinitions = stepDefinitions.filter {
-      updatedTask.evaluationStepDefinitions.find { evaluationStepDefinition -> it == evaluationStepDefinition } == null
-    }
-    updatedTask.evaluationStepDefinitions.addAll(newStepDefinitions)
-
-    return updatedTask
-  }
-
-  private fun updateExistingEvaluationStepDefinitions(task: Task, newStepDefinitions: Collection<EvaluationStepDefinition>) {
-    task.evaluationStepDefinitions.forEach { existingStepDefinition ->
-      val stepDefinition = newStepDefinitions.find { it == existingStepDefinition }
-
-      if (stepDefinition != null) {
-        evaluationService.updateEvaluationStepDefinition(existingStepDefinition, stepDefinition.title, stepDefinition.active, stepDefinition.options)
-      } else {
-        try {
-          evaluationService.deleteEvaluationStepDefinition(existingStepDefinition)
-        } catch (e: DataIntegrityViolationException) {
-          // The step cannot be deleted if it has been used for evaluation already
-          // Deactivate it for future evaluation instead
-          existingStepDefinition.active = false
-        }
-      }
-    }
   }
 
   /**
@@ -173,7 +101,6 @@ class TaskTarService : BaseService() {
         val runner = evaluationService.getEvaluationRunner(it.step)
         val title = it.title ?: runner.getDefaultTitle()
         val stepDefinition = EvaluationStepDefinition(task, runner.getName(), index, title, it.options)
-        stepDefinition.id = UuidUtil.parse(it.id) ?: stepDefinition.id
         evaluationService.validateRunnerOptions(stepDefinition)
         stepDefinition
       }
@@ -188,13 +115,11 @@ class TaskTarService : BaseService() {
   /**
    * Creates and saves multiple tasks from the given tar.
    * The tar has to contain the individual tasks as tar archives themselves.
-   * Tasks that already exist, but are newer in the archive are updated,
-   * new tasks are created and saved to the repository.
    *
    * @param tarContent the tar containing multiple tasks as tar archives
    * @param owner the owner of the tasks
    * @param assignment the assignment the tasks belong to, if any
-   * @return the created or updated tasks.
+   * @return the created tasks.
    */
   fun createMultipleFromTar(tarContent: ByteArray, owner: User, assignment: Assignment? = null): List<Task> {
     val input = TarArchiveInputStream(ByteArrayInputStream(tarContent))
@@ -260,20 +185,17 @@ class TaskTarService : BaseService() {
     }
 
     val definition = TaskDefinition(
-        title = task.title,
-        id = task.id.toString(),
-        description = task.body,
-        hidden = task.hiddenFiles,
-        protected = task.protectedFiles,
-        evaluation = task.evaluationStepDefinitions.map {
+        task.title,
+        task.body,
+        task.hiddenFiles,
+        task.protectedFiles,
+        task.evaluationStepDefinitions.map {
           EvaluationDefinition(
-              step = it.runnerName,
-              options = it.options,
-              title = it.title,
-              id = it.id.toString()
+              it.runnerName,
+              it.options,
+              it.title
           )
-        },
-        updatedAt = task.updatedAt.toString()
+        }
     ).let { yamlMapper.writeValueAsBytes(it) }
 
     tar.putArchiveEntry(TarArchiveEntry("codefreak.yml").also { it.size = definition.size.toLong() })
