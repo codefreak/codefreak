fd9b0a311fbf9dccff3b42c047ebb87c1a82c85f
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-02-09 20:46:15 +0100

    enhanced the Body and added some generic functions

--- src/main/kotlin/org/codefreak/codefreak/repository/EvaluationStepRepository.kt
+++ src/main/kotlin/org/codefreak/codefreak/repository/EvaluationStepRepository.kt
@@ -1,9 +1,15 @@
 package org.codefreak.codefreak.repository
 
-import java.util.UUID
+import org.codefreak.codefreak.entity.Evaluation
 import org.codefreak.codefreak.entity.EvaluationStep
+import org.codefreak.codefreak.entity.GradeDefinition
+import org.codefreak.codefreak.entity.PointsOfEvaluationStep
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface EvaluationStepRepository : CrudRepository<EvaluationStep, UUID>
+interface EvaluationStepRepository : CrudRepository<EvaluationStep, UUID>{
+  fun findAllByEvaluation(evaluation: Evaluation) : MutableList<EvaluationStep>
+  fun findAllByGradeDefinition(gradeDefinition : GradeDefinition) : MutableList<EvaluationStep>
+}
