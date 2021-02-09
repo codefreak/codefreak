380a12b92132768a8d796ced6746b89e4ef93ba6
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-02-09 20:45:13 +0100

    Added function to find an Evaluation by one of its EvaluationSteps.

--- src/main/kotlin/org/codefreak/codefreak/repository/EvaluationRepository.kt
+++ src/main/kotlin/org/codefreak/codefreak/repository/EvaluationRepository.kt
@@ -1,8 +1,11 @@
 package org.codefreak.codefreak.repository
 
+import org.codefreak.codefreak.entity.Answer
 import java.util.Optional
 import java.util.UUID
 import org.codefreak.codefreak.entity.Evaluation
+import org.codefreak.codefreak.entity.EvaluationStep
+import org.codefreak.codefreak.entity.User
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
@@ -10,4 +13,12 @@ import org.springframework.stereotype.Repository
 interface EvaluationRepository : CrudRepository<Evaluation, UUID> {
   fun findFirstByAnswerIdOrderByCreatedAtDesc(answerId: UUID): Optional<Evaluation>
   fun findFirstByAnswerIdAndFilesDigestOrderByCreatedAtDesc(answerId: UUID, digest: ByteArray): Optional<Evaluation>
+
+
+  /**
+   * function to receive the Evaluation from its EvaluationStep.
+   * There is a case of Backtracking in the process of automatic grade calculation
+   */
+  fun findByEvaluationSteps(evaluationStep: EvaluationStep) : Optional<Evaluation>
+
 }
