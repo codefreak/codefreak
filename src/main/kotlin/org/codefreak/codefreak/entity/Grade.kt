c2486ca8f9e2e5ce8d82e668722307721ffbc98a
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-02-09 20:20:24 +0100

    Grade Entity which is connection to an Evaluation Entity. It is also part of the Answer Entity and holds n PointsOfEvaluationStep Entities.
    Provides a Grade for each evaluation and as a Set in the Answer Entity it help to pick the best of the set.

---
+++ src/main/kotlin/org/codefreak/codefreak/entity/Grade.kt
@@ -0,0 +1,44 @@
+package org.codefreak.codefreak.entity
+
+import javax.persistence.*
+
+
+@Entity
+class Grade(
+
+  /**
+   * Every Evaluation can have a grade. Depends if enabled or not. Later we can pick the a result
+   */
+  @OneToOne
+  @JoinColumn(name = "evaluation", referencedColumnName = "id")
+  var evaluation : Evaluation,
+
+  /**
+   * A Grade is always part of a set in answer entity.
+   */
+  @ManyToOne
+  var answer : Answer
+
+) : BaseEntity(){
+
+
+
+  /**
+   * Percentage of 100 how much a student reached. defined as float because value will be:  1>=x>=0
+   */
+  var gradePercentage : Float = 0f
+
+  /**
+   * Checks if a Grade is actually calculated. Otherwise a formatted zero might be shown in frontend due to a possible
+   * grade applied to their respective evaluation but no grade was calculated.
+   */
+  var calculated : Boolean= false
+
+
+  /**
+   * A Grade is calculated of multiply PointsOfEvaluationSteps.
+   */
+  @OneToMany(mappedBy = "grade", cascade = [CascadeType.ALL],orphanRemoval = true, fetch = FetchType.LAZY)
+  var pointsOfEvaluationStep = mutableSetOf<PointsOfEvaluationStep>()
+
+}
