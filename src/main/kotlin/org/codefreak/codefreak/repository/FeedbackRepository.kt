dfc8098e04a79f957034ae739472e5e36f0987bc
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-02-09 20:48:02 +0100

    added some function to access two cases which are required to get the flawed and errored feedbacks

--- src/main/kotlin/org/codefreak/codefreak/repository/FeedbackRepository.kt
+++ src/main/kotlin/org/codefreak/codefreak/repository/FeedbackRepository.kt
@@ -1,9 +1,26 @@
 package org.codefreak.codefreak.repository
 
+import org.codefreak.codefreak.entity.EvaluationStep
 import java.util.UUID
 import org.codefreak.codefreak.entity.Feedback
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FeedbackRepository : CrudRepository<Feedback, UUID>
+interface FeedbackRepository : CrudRepository<Feedback, UUID>{
+
+
+  /**
+   * function to receive all flawed feedbacks of an evaluationstep
+   * Required to identify errors and to evaluate
+   */
+  fun findByEvaluationStepAndStatusAndSeverityNot(evaluationStep : EvaluationStep, status : Feedback.Status, severity : Feedback.Severity) : MutableList<Feedback>
+
+  /**
+   * function to receive all EvaluationSteps which dont have a specific status. E. g. if we ask for Not Status.Info
+   * we would get all flawed Feedbacks for this EvaluationStep. Used for PointsOfEvaluationStep value calculation
+   */
+  fun findByEvaluationStepAndStatusNot(evaluationStep: EvaluationStep,status: Feedback.Status) : MutableList<Feedback>
+
+
+}
