87e24aeb67f8238804e1224650ed41f62d3e44f8
Felix <felix.woelk@fh-kiel.de>
2021-03-14 10:07:51 +0100

    Zwischenstand

--- client/src/pages/evaluation/EditEvaluationPage.tsx
+++ client/src/pages/evaluation/EditEvaluationPage.tsx
@@ -317,9 +317,10 @@ const EditEvaluationPage: React.FC<{ taskId: string }> = ({ taskId }) => {
       <Alert
         message={
           <>
-            Here you can configure the automatic evaluation. Only you as a
-            teacher can see this. To try it out, enable testing mode and start
-            the evaluation.
+            You can configure the automatic evaluation here. The configuration
+            is only visible to teachers. To check the configuration, enable
+            "Testing Mode" on the top of the page and click the "Start Evaluation"
+            button on the top right.
             <br />
             The order of evaluation steps determines the order of the feedback
             displayed to students.
--- client/src/pages/task/TaskDetailsPage.tsx
+++ client/src/pages/task/TaskDetailsPage.tsx
@@ -11,7 +11,7 @@ import {
   Button,
   Card,
   Checkbox,
-  Col,
+  Col, Collapse,
   Empty,
   Input,
   List,
@@ -22,7 +22,7 @@ import {
 } from 'antd'
 import { CheckboxChangeEvent } from 'antd/lib/checkbox'
 import { JSONSchema6 } from 'json-schema'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Link } from 'react-router-dom'
 import AsyncPlaceholder from '../../components/AsyncContainer'
@@ -42,8 +42,12 @@ import { shorten } from '../../services/short-id'
 import { makeUpdater } from '../../services/util'
 import EditEvaluationPage from '../evaluation/EditEvaluationPage'
 import useSystemConfig from '../../hooks/useSystemConfig'
+import CropContainer from "../../components/CropContainer";
+import './TaskDetailsPage.less'
 
 const { TabPane } = Tabs
+
+const { Panel } = Collapse
 
 const renderFilePattern = (pattern: string) => (
   <List.Item>
@@ -155,17 +159,212 @@ const TaskDetailsPage: React.FC<{ editable: boolean }> = ({ editable }) => {
         {details}
       </TabPane>
       <TabPane tab="Edit Details" key="/edit">
-        <Card title="Instructions">
-          {task.body || editable ? (
-            <EditableMarkdown
-              content={task.body}
-              editable={editable}
-              onSave={updater('body')}
-            />
-          ) : (
-            <Empty description="This task has no extra instructions. Take a look at the provided files." />
-          )}
-        </Card>
+        <Collapse>
+          <Panel header="Instructions" key="">
+            {task.body || editable ? (
+
+                  <EditableMarkdown
+                    content={task.body}
+                    editable={editable}
+                    onSave={updater('body')}
+                  />
+
+
+            ) : (
+              <Empty description="This task has no extra instructions. Take a look at the provided files." />
+            )}
+          </Panel>
+          <Panel header={
+            <div className="collapse-panel-title">
+              <span>Files</span>
+              <span>
+                <Link
+                to={'/ide/task/' + shorten(task.id)}
+                target={'task-ide-' + task.id}
+                >
+                <Button
+                type="primary"
+                icon={<EditOutlined />}
+                disabled={assignmentOpen && !sureToEditFiles}
+                >
+                Edit task files in IDE
+                </Button>
+                </Link>{' '}
+              {task.assignment?.id && (
+                <StartSubmissionEvaluationButton
+                assignmentId={task.assignment.id}
+                invalidateTask={task.id}
+                disabled={assignmentOpen && !sureToEditFiles}
+                type="primary"
+                icon={<SyncOutlined />}
+                >
+                Evaluate all answers of this task
+                </StartSubmissionEvaluationButton>
+                )}
+              </span>
+            </div>
+          } key="/edit">
+            {assignmentOpen ? (
+              <Alert
+                style={{ marginBottom: 16 }}
+                message="Warning"
+                description={
+                  <>
+                    <p>
+                      The assignment is already open. If you make changes to
+                      files, they do not affect students that already started to
+                      work on this task. Only students that start the task after
+                      the change will get the updated files. Hidden and protected
+                      files are updated for everyone but only in new evaluations.
+                      Past evaluations are not affected.
+                    </p>
+                    <p>
+                      After you made changes to the files please click the
+                      evaluation button to check all answers again!
+                    </p>
+                    <p>
+                      <Checkbox onChange={onSureToEditFilesChange}>
+                        I understand this and want to do it anyway
+                      </Checkbox>
+                    </p>
+                  </>
+                }
+                type="warning"
+                showIcon
+              />
+            ) : null}
+            <Row gutter={16}>
+              <Col span={12}>
+                <List
+                  size="small"
+                  header={
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                      }}
+                    >
+                    <span>
+                      <b>Hidden files</b>{' '}
+                      <Tooltip
+                        title={
+                          'Patterns of files that should be hidden from students. Matching files are only included for evaluation. If matching files are created by students, they are ignored for evaluation.'
+                        }
+                        placement="bottom"
+                      >
+                        <InfoCircleFilled />
+                      </Tooltip>
+                    </span>
+                      <JsonSchemaEditButton
+                        title="Edit hidden files"
+                        extraContent={filePatternHelp}
+                        schema={filePatternSchema}
+                        value={task.hiddenFiles}
+                        onSubmit={updater('hiddenFiles')}
+                      />
+                    </div>
+                  }
+                  bordered
+                  dataSource={task.hiddenFiles}
+                  renderItem={renderFilePattern}
+                />
+              </Col>
+              <Col span={12}>
+                <List
+                  size="small"
+                  header={
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                      }}
+                    >
+                    <span>
+                      <b>Protected files</b>{' '}
+                      <Tooltip
+                        title={
+                          'Patterns of files that should be read-only. Students will be able to see matching files but modifications are ignored for evaluation. Non-existent files can be protected to prevent their creation.'
+                        }
+                        placement="bottom"
+                      >
+                        <InfoCircleFilled />
+                      </Tooltip>
+                    </span>
+                      <JsonSchemaEditButton
+                        title="Edit protected files"
+                        extraContent={filePatternHelp}
+                        schema={filePatternSchema}
+                        value={task.protectedFiles}
+                        onSubmit={updater('protectedFiles')}
+                      />
+                    </div>
+                  }
+                  bordered
+                  dataSource={task.protectedFiles}
+                  renderItem={renderFilePattern}
+                />
+              </Col>
+            </Row>
+
+          </Panel>
+          <Panel header={"Evaluation"} key="/edit-evaluation">
+            <EditEvaluationPage taskId={task.id} />
+          </Panel>
+          <Panel header={"Online IDE"} key={4}>
+            <Row gutter={16}>
+              <Col span={12}>
+                <h3>Image</h3>
+                <p>
+                  Optionally, you can specify a custom Docker image for the
+                  student Online IDE. You will most likely <em>not</em> need this!
+                  Read more about custom IDE images{' '}
+                  <CodefreakDocsLink category="for-teachers" page="ide">
+                    here
+                  </CodefreakDocsLink>
+                  .
+                </p>
+                <p>
+                  Leave blank to use the default image{' '}
+                  <code>{defaultIdeImage}</code>.
+                </p>
+                <Input.Search
+                  style={{
+                    maxWidth: 400
+                  }}
+                  defaultValue={task.ideImage || ''}
+                  placeholder="e.g. foo/bar:latest"
+                  allowClear
+                  enterButton={<SaveOutlined />}
+                  onSearch={updater('ideImage')}
+                />
+              </Col>
+              <Col span={12}>
+                <h3>CMD / Arguments</h3>
+                <p>
+                  You can customize the CMD on the container to alter either the
+                  container CMD or pass additional arguments to the container.
+                  Only use this if you know what you are doing.
+                  <br />
+                  <strong>Warning:</strong> These values are NOT parameters for{' '}
+                  <code>docker run</code>!
+                </p>
+                <Input.Search
+                  style={{
+                    maxWidth: 400
+                  }}
+                  defaultValue={task.ideArguments || ''}
+                  placeholder="--option=value --verbose"
+                  allowClear
+                  enterButton={<SaveOutlined />}
+                  onSearch={updater('ideArguments')}
+                />
+              </Col>
+            </Row>
+          </Panel>
+
+        </Collapse>
 
         <Card title="Files" style={{ marginTop: 16 }}
               extra={
@@ -195,6 +394,7 @@ const TaskDetailsPage: React.FC<{ editable: boolean }> = ({ editable }) => {
                   )}
                   </>
               }>
+          <CropContainer maxHeight={50}>
           {assignmentOpen ? (
             <Alert
               style={{ marginBottom: 16 }}
@@ -298,6 +498,7 @@ const TaskDetailsPage: React.FC<{ editable: boolean }> = ({ editable }) => {
               />
             </Col>
           </Row>
+          </CropContainer>
         </Card>
         <Card
           title="Online IDE"
