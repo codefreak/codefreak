8d522ffb1a0902d93f79d76e5eb6a1d99ba61bb2
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-04-04 22:09:21 +0200

    - refetches its own value after a mutation than the whole Query which was done previously.

--- client/src/components/autograder/EditNickname.tsx
+++ client/src/components/autograder/EditNickname.tsx
@@ -3,16 +3,15 @@ import React, { useState } from 'react'
 import { extractTargetValue } from '../../services/util'
 import useAuthenticatedUser from '../../hooks/useAuthenticatedUser'
 import {
+  GetUserAliasByUserIdDocument,
   useGetUserAliasByUserIdQuery,
   UserAliasInput,
   useUpdateUserAliasMutation
 } from '../../generated/graphql'
 import AsyncPlaceholder from '../AsyncContainer'
-import { messageService } from '../../services/message'
 
 const EditNickname: React.FC<{
   editable: boolean
-  onChange: any
 }> = props => {
   const [modalVisible, setModalVisible] = useState(false)
   const [newAlias, setNewAlias] = useState<string>()
@@ -23,15 +22,7 @@ const EditNickname: React.FC<{
     variables: { userId }
   })
 
-  const [updateUserAlias] = useUpdateUserAliasMutation({
-    onCompleted: () => {
-      result.refetch()
-      messageService.success('Nickname may have Updated')
-    },
-    onError: () => {
-      messageService.error('Nickname already exists')
-    }
-  })
+  const [updateUserAlias] = useUpdateUserAliasMutation()
 
   if (result.data === null || result.data === undefined) {
     return <AsyncPlaceholder result={result} />
@@ -40,22 +31,30 @@ const EditNickname: React.FC<{
 
   const input: UserAliasInput = {
     id: userAlias.id,
-    alias: newAlias ? newAlias : userAlias.alias
+    alias: newAlias ? newAlias : userAlias.alias!!
   }
 
   const showModal = () => {
-    setNewAlias(userAlias.alias)
+    setNewAlias(userAlias.alias!!)
     setModalVisible(true)
   }
   const hideModal = () => setModalVisible(false)
 
   const submit = () => {
     if (newAlias && newAlias.trim()) {
-      props.onChange()
       hideModal()
-      return updateUserAlias({ variables: { input } })
+      return updateUserAlias({
+        variables: { input },
+        refetchQueries: [
+          {
+            query: GetUserAliasByUserIdDocument,
+            variables: { userId }
+          }
+        ]
+      })
     }
   }
+
   if (!props.editable) {
     return <>{userAlias.alias}</>
   }
