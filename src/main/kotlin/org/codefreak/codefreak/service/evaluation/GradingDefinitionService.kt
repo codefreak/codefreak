ad019a136eb4da65ccfc91de09c1d89493750326
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-04-04 23:10:41 +0200

    - removed unused functions and added EntityNotFoundExceptions

---
+++ src/main/kotlin/org/codefreak/codefreak/service/evaluation/GradingDefinitionService.kt
@@ -0,0 +1,68 @@
+package org.codefreak.codefreak.service.evaluation
+
+import java.util.UUID
+import org.codefreak.codefreak.entity.GradingDefinition
+import org.codefreak.codefreak.repository.EvaluationStepDefinitionRepository
+import org.codefreak.codefreak.repository.EvaluationStepRepository
+import org.codefreak.codefreak.repository.GradingDefinitionRepository
+import org.codefreak.codefreak.service.BaseService
+import org.codefreak.codefreak.service.EntityNotFoundException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class GradingDefinitionService : BaseService() {
+
+  @Autowired
+  private lateinit var gradingDefinitionRepository: GradingDefinitionRepository
+
+  @Autowired
+  private lateinit var evaluationStepDefinitionRepository: EvaluationStepDefinitionRepository
+
+  @Autowired
+  private lateinit var evaluationStepRepository: EvaluationStepRepository
+
+  @Autowired
+  private lateinit var pointsOfEvaluationStepService: PointsOfEvaluationStepService
+
+  fun save(gradingDefinition: GradingDefinition): GradingDefinition = gradingDefinitionRepository.save(gradingDefinition)
+
+  @Transactional
+  fun findGradingDefinition(gradingDefinitionId: UUID): GradingDefinition {
+    return gradingDefinitionRepository.findById(gradingDefinitionId).orElseThrow {
+      EntityNotFoundException("GradingDefinition $gradingDefinitionId could not be found")
+    }
+  }
+
+  @Transactional
+  fun findGradingDefinitionByEvaluationStepDefinitionId(evaluationStepDefinitionId: UUID): GradingDefinition {
+    return gradingDefinitionRepository.findByEvaluationStepDefinitionId(evaluationStepDefinitionId).orElseThrow {
+      EntityNotFoundException("GradingDefinition by evaluationStepDefinitionId $evaluationStepDefinitionId could not be found")
+    }
+  }
+
+  fun findByEvaluationStepDefinition(evaluationStepDefinitionId: UUID): GradingDefinition {
+    return evaluationStepDefinitionRepository.findById(evaluationStepDefinitionId).orElseThrow {
+      EntityNotFoundException("GradingDefinition by evaluationStepDefinitionId $evaluationStepDefinitionId could not be found")
+    }.gradingDefinition!!
+  }
+
+  fun updateGradingDefinition(gradingDefinition: GradingDefinition, active: Boolean?, maxPoints: Float?, minorMistakePenalty: Float?, majorMistakePenalty: Float?, criticalMistakePenalty: Float?): GradingDefinition {
+    active?.let { gradingDefinition.active = active }
+    maxPoints?.let { gradingDefinition.maxPoints = maxPoints }
+    minorMistakePenalty?.let { gradingDefinition.minorMistakePenalty = minorMistakePenalty }
+    majorMistakePenalty?.let { gradingDefinition.majorMistakePenalty = majorMistakePenalty }
+    criticalMistakePenalty?.let { gradingDefinition.criticalMistakePenalty = criticalMistakePenalty }
+    val def = save(gradingDefinition)
+    pointsOfEvaluationStepService.recalculatePoints(gradingDefinition)
+    return def
+  }
+
+
+  @Transactional
+  fun findByPointsOfEvaluationStepId(pointsOfEvaluationStepId: UUID): GradingDefinition {
+    val step = evaluationStepRepository.findByPointsId(pointsOfEvaluationStepId).get()
+    return step.definition.gradingDefinition!!
+  }
+}
