48ca6a5c4e8cbbf666505af8ba87ee0159ee943a
Sjtim <52971601+Sjtim@users.noreply.github.com>
2021-04-04 23:00:38 +0200

    - removed unnessecary relation and as recommended removed the calculated variable and made the gradePercentage nullable

--- src/main/kotlin/org/codefreak/codefreak/entity/Grade.kt
+++ src/main/kotlin/org/codefreak/codefreak/entity/Grade.kt
@@ -1,11 +1,8 @@
 package org.codefreak.codefreak.entity
 
-import javax.persistence.CascadeType
 import javax.persistence.Entity
-import javax.persistence.FetchType
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
 import javax.persistence.OneToOne
 
 @Entity
@@ -29,17 +26,5 @@ class Grade(
   /**
    * Percentage of 100 how much a student reached. defined as float because value will be:  1>=x>=0
    */
-  var gradePercentage: Float = 0f
-
-  /**
-   * Checks if a Grade is actually calculated. Otherwise a formatted zero might be shown in frontend due to a possible
-   * grade applied to their respective evaluation but no grade was calculated.
-   */
-  var calculated: Boolean = false
-
-  /**
-   * A Grade is calculated of multiply PointsOfEvaluationSteps.
-   */
-  @OneToMany(mappedBy = "grade", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
-  var pointsOfEvaluationStep = mutableSetOf<PointsOfEvaluationStep>()
+  var gradePercentage: Float? = null
 }
