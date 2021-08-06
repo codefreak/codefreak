a52249380414c2b57198e677f69c568506130095
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2021-08-06 16:28:25 +0200

    Only clean-up file-collections when an in-memory database is in use

--- src/main/kotlin/org/codefreak/codefreak/service/CleanupDevFileSystemService.kt
+++ src/main/kotlin/org/codefreak/codefreak/service/CleanupDevFileSystemService.kt
@@ -2,6 +2,7 @@ package org.codefreak.codefreak.service
 
 import java.util.UUID
 import javax.annotation.PreDestroy
+import javax.sql.DataSource
 import org.codefreak.codefreak.Env
 import org.codefreak.codefreak.repository.AnswerRepository
 import org.codefreak.codefreak.repository.TaskRepository
@@ -29,10 +30,18 @@ class CleanupDevFileSystemService {
   @Autowired
   lateinit var answerRepository: AnswerRepository
 
+  @Autowired
+  lateinit var dataSource: DataSource
+
   private val log = LoggerFactory.getLogger(this::class.java)
 
   @PreDestroy
   fun cleanupFileCollections() {
+    if (!isInMemoryDatabase(dataSource)) {
+      log.info("Not cleaning-up any file collections since no in-memory database is in use")
+      return
+    }
+
     val fileCollectionIds = mutableListOf<UUID>()
 
     taskRepository.findAll().forEach { fileCollectionIds.add(it.id) }
@@ -43,5 +52,16 @@ class CleanupDevFileSystemService {
     }
 
     log.info("Cleaned up ${fileCollectionIds.size} file collections from disk")
+  }
+
+  private fun isInMemoryDatabase(dataSource: DataSource): Boolean {
+    val url = dataSource.connection.metaData.url
+
+    val isInMemoryH2Database = url.startsWith("jdbc:h2:mem")
+    val isInMemoryHsqlDatabase = url.startsWith("jdbc:hsqldb:mem")
+    val isInMemoryDerbyDatabase = url.startsWith("jdbc:derby:memory")
+    val isInMemorySqliteDatabase = url.startsWith("jdbc:sqlite:memory")
+
+    return isInMemoryH2Database || isInMemoryHsqlDatabase || isInMemoryDerbyDatabase || isInMemorySqliteDatabase
   }
 }
