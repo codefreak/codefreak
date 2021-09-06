cd09112387cfca2370b557548e975784ca691078
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2021-09-06 12:19:45 +0200

    Fix jest setup

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from 'react-dom'
 import App from './App'
+import { MockedProvider } from '@apollo/client/testing'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <MockedProvider>
+      <App />
+    </MockedProvider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
