f65e8ebb3bddbf5156e2ee47c60428b2b946fc71
Henning Kasch <henning.kasch@gmail.com>
2021-10-16 14:14:29 +0200

    WS client connection tweaks

--- src/main/kotlin/org/codefreak/codefreak/service/workspace/WorkspaceClient.kt
+++ src/main/kotlin/org/codefreak/codefreak/service/workspace/WorkspaceClient.kt
@@ -43,7 +43,6 @@ class WorkspaceClient(
 
   private val baseUri = URI(baseUrl)
   private val requestFactory = OkHttpClient.Builder()
-    .retryOnConnectionFailure(false)
     .also {
       // Provide authentication token via Authorization header
       if (authToken != null) {
@@ -61,8 +60,6 @@ class WorkspaceClient(
 
   val apolloClient = ApolloClient(
     networkTransport = WebSocketNetworkTransport(
-      connectionAcknowledgeTimeoutMs = 60_000, // 1min
-      idleTimeoutMillis = 3600_000, // 1h
       serverUrl = buildWorkspaceUri(path = "/graphql"),
       protocol = GraphQLWsProtocol(),
       webSocketEngine = DefaultWebSocketEngine(
@@ -96,7 +93,6 @@ class WorkspaceClient(
     val request = Request.Builder()
       .post(body)
       .url(buildWorkspaceUri(path = "/files-tar"))
-      .header("Connection", "close")
       .build()
     requestFactory.newCall(request).execute { response ->
       if (response.code() != 201) {
@@ -116,7 +112,6 @@ class WorkspaceClient(
         path = "/files-tar",
         query = filter?.run { "filter=$filter" }
       ))
-      .header("Connection", "close")
       .build()
     requestFactory.newCall(request).execute { response ->
       val body = response.body() ?: throw IllegalStateException("Downloading files returned no body")
@@ -154,7 +149,6 @@ class WorkspaceClient(
     val request = Request.Builder()
       .get()
       .url(buildWorkspaceUri(path = "/actuator/health/readiness"))
-      .header("Connection", "close")
       .build()
     try {
       requestFactory.newCall(request).execute { response ->
@@ -181,7 +175,6 @@ class WorkspaceClient(
     val request = Request.Builder()
       .get()
       .url(buildWorkspaceUri(path = "/actuator/metrics/http.websocket.connections"))
-      .header("Connection", "close")
       .build()
     requestFactory.newCall(request).execute { response ->
       val body = response.body() ?: throw IllegalStateException("Metric response has no body")
@@ -268,7 +261,6 @@ class WorkspaceClient(
 
   /**
    * Automatically closes the body after consuming the response
-   * TODO: This is only working when "Connection close" header is sent.
    */
   private inline fun <T> Call.execute(responseConsumer: (response: Response) -> T): T = execute().use(responseConsumer)
 
