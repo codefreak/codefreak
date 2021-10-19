71c80f6bf4ef2bba360d810e3533ab3a8d2b3866
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2021-10-19 16:16:06 +0200

    Delete obsolete file and add the security configuration to file-based tests

--- cloud-companion/src/test/kotlin/org/codefreak/cloud/companion/web/FileBasedTest.kt
+++ cloud-companion/src/test/kotlin/org/codefreak/cloud/companion/web/FileBasedTest.kt
@@ -4,6 +4,7 @@ import kotlin.io.path.createDirectories
 import org.apache.commons.io.FileUtils
 import org.codefreak.cloud.companion.CompanionConfig
 import org.codefreak.cloud.companion.FileService
+import org.codefreak.cloud.companion.security.SecurityConfiguration
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
@@ -14,7 +15,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @ExtendWith(SpringExtension::class)
-@Import(FileService::class, CompanionConfig::class)
+@Import(FileService::class, CompanionConfig::class, SecurityConfiguration::class)
 @ActiveProfiles("test")
 abstract class FileBasedTest {
   @Autowired
