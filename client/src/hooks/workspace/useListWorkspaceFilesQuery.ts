a8628886938e7a8f6d4f5e5d6a486fd4a1f2de50
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2021-10-25 13:24:19 +0200

    Add jsdoc for useListWorkspaceFileQuery.ts

--- client/src/hooks/workspace/useListWorkspaceFilesQuery.ts
+++ client/src/hooks/workspace/useListWorkspaceFilesQuery.ts
@@ -2,6 +2,12 @@ import { useQuery } from 'react-query'
 import useWorkspace from './useWorkspace'
 import { Client } from 'graphql-ws'
 
+/**
+ * Returns the GraphQL query for listing the files of a given path as a string
+ *
+ * @param path the path to list files in
+ * @returns the GraphQL query for listing the files of a given path as a string
+ */
 const LIST_FILES = (path: string) => `
   query ListFiles {
     listFiles(path: "${path}") {
@@ -13,19 +19,39 @@ const LIST_FILES = (path: string) => `
   }
 `
 
-type ListFilesType = {
+/**
+ * The type of object returned by the ListFiles query
+ */
+type ListFilesReturnType = {
   listFiles: FileSystemNode[]
 }
 
+/**
+ * Represents a file-systen node with a path and an optional size if it is a file
+ */
 export type FileSystemNode = {
+  /**
+   * The path of the node
+   */
   path: string
+
+  /**
+   * The optional size of the node if it is a file
+   */
   size?: number
 }
 
+/**
+ * Queries the file-system nodes in a path
+ *
+ * @param path the path
+ * @param graphqlWebSocketClient the GraphQL client to use for the query
+ * @returns a promise with the file-system nodes in the path
+ */
 export const listFiles = (path: string, graphqlWebSocketClient: Client) =>
   new Promise<FileSystemNode[]>((resolve, reject) => {
     let result: FileSystemNode[] = []
-    graphqlWebSocketClient.subscribe<ListFilesType>(
+    graphqlWebSocketClient.subscribe<ListFilesReturnType>(
       { query: LIST_FILES(path) },
       {
         next: value => {
@@ -39,6 +65,11 @@ export const listFiles = (path: string, graphqlWebSocketClient: Client) =>
     )
   })
 
+/**
+ * Queries the file-system nodes in the root path of a workspace
+ *
+ * @returns a query for the file-system nodes in the root path of a workspace
+ */
 const useListWorkspaceFilesQuery = () => {
   const { baseUrl, graphqlWebSocketClient } = useWorkspace()
   return useQuery(
